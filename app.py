d723c8a4efae22936d4fb6f8fd0d038cbc1c6dba
alexytl <147929136+alexytl@users.noreply.github.com>
2025-01-01 19:58:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,9 @@ path = ".devcontainer/tank.csv"
 
 def main():
     st.title("ECHA Stats App")
-    """
-    file = st.file_uploader("Upload CSV File", type="csv")
     
-    if file:
-        with open("echa.csv", "wb") as f:
-            f.write(file.getbuffer())
-        csv_path = "echa.csv"
-        st.success("Using Uploaded CSV File")
-    else:
-        csv_path = "tank.csv"
-        st.info("Using Default CSV")
-    """
     game_reader = GameReader2()
     game_reader.read_csv(path)
-        
     games = game_reader.get_games()
     teams = game_reader.get_teams()
     standings = Standings2(teams)
