db7bad9919b05da57d95d8b141329a328fc1a690
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-04 23:55:34 +0900

    Update 9_Roadmap.py

--- pages/9_Roadmap.py
+++ pages/9_Roadmap.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
   st.title("Content Roadmap")
   st.header("Updates")
-  st.markdown("2-4: Created multipage app, added Playoff page and Playoff Picture section")
+  st.markdown("2-4: Created multipage app, added Playoffs page and Playoff Picture section")
   st.header("In Progress")
   st.markdown("Sooner: Playoff Bracket, Playoff Matchup Guide, Home/Away Splits, First/Second Matchup Splits, Away Rink Stats, Enhanced Graphing Capabilities")
   st.markdown("Later: Player Statistics, Predictive Model")
