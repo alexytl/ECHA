28d3bff115e745eb02551bf86f9241814efacb8b
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-04 19:00:45 +0900

    Update 2_Test.py

--- pages/2_Test.py
+++ pages/2_Test.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import pandas as pd
+import os
 
-path = "../.devcontainer/dex.csv"
-
-# Test reading CSV
-try:
-    data = pd.read_csv(path)
-    st.write("CSV loaded successfully!")
-    st.write(data.head())  # Display first few rows of data
-except Exception as e:
-    st.write(f"Error loading CSV: {e}")
+st.write(f"Current working directory: {os.getcwd()}")
