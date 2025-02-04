dadd8777f5a986df3116f8d3203357096d53a595
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-04 19:07:18 +0900

    Update 1_Playoffs.py

--- pages/1_Playoffs.py
+++ pages/1_Playoffs.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sys
-import os
+from team import Team
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from game_reader_2 import GameReader2
