3b13da973ee95a3be32a145b4ba82f7894efc23f
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-04 19:02:37 +0900

    Update 2_Test.py

--- pages/2_Test.py
+++ pages/2_Test.py
@@ -1,7 +1,8 @@
 import os
 import streamlit as st
 
-path = ".devcontainer/dex.csv"
-file_exists = os.path.exists(path)
-
-st.write(f"Does the file exist? {file_exists}")
+try:
+    with open(".devcontainer/dex.csv", 'r', encoding='utf-8') as file:
+        st.write("File is accessible!")
+except Exception as e:
+    st.write(f"Error opening file: {e}")
