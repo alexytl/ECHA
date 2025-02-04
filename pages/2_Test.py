171bbedc83258631e6c2b55ca5970ebbb532eac5
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-04 19:03:53 +0900

    Update 2_Test.py

--- pages/2_Test.py
+++ pages/2_Test.py
@@ -1,9 +1,17 @@
-import pandas as pd
 import streamlit as st
+from game_reader_2 import GameReader2
 
-try:
-    data = pd.read_csv(".devcontainer/dex.csv")
-    st.write("CSV loaded successfully!")
-    st.write(data.head())  # Show first few rows
-except Exception as e:
-    st.write(f"Error loading CSV: {e}")
+path = ".devcontainer/dex.csv"
+
+def main():
+    st.title("Playoffs")
+
+    game_reader = GameReader2()
+    game_reader.read_csv(path)  
+    teams = game_reader.get_teams()
+
+    # Debugging: Show teams
+    st.write("Teams loaded:", teams)
+
+if __name__ == "__main__":
+    main()
