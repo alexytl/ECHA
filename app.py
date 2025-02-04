b39ce201a5bcf41d082cb9d735874b63611b6144
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-04 13:12:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     st.markdown("Last Updated: February 4, 2025")
 
     image_url = ".devcontainer/logo.png"
-    st.logo(image_url)
+    st.logo(image_url, large)
     
     #boring stuff
     game_reader = GameReader2()
