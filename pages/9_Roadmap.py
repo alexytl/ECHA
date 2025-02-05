36f88eb3d113763dda831453d022f964d239ebb8
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-05 12:39:22 +0900

    Update 9_Roadmap.py

--- pages/9_Roadmap.py
+++ pages/9_Roadmap.py
@@ -5,7 +5,7 @@ def main():
   st.header("Updates")
   st.markdown("2-4: Created multipage app, added Playoffs page and Playoff Picture section")
   st.header("Up Next")
-  st.markdown("Sooner: Playoff Bracket, Playoff Matchup Guide, Pages for Graphing and Advanced Statistics, Home/Away Splits, First/Second Matchup Splits, Away Rink Stats, Enhanced Graphing Capabilities")
+  st.markdown("Sooner: Playoff Bracket, Playoff Matchup Guide, Pages for Schedule, Graphing and Advanced Statistics, Home/Away Splits, First/Second Matchup Splits, Away Rink Stats, Enhanced Graphing Capabilities")
   st.markdown("Later: Player Statistics, Predictive Model")
 
 if __name__ == "__main__":
