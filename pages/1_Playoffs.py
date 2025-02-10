df0abe5086b2030a303dcc33d61f2860ae7b73fe
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-10 12:42:05 +0900

    Update 1_Playoffs.py

--- pages/1_Playoffs.py
+++ pages/1_Playoffs.py
@@ -4,7 +4,7 @@ from team import Team
 from game_reader_2 import GameReader2
 import os
 
-path = os.path.join(os.getcwd(), '.devcontainer', 'savage.csv')
+path = os.path.join(os.getcwd(), '.devcontainer', 'hurts.csv')
 
 
 def main():
