e1957191df983e49e69ea026e4ad0ea88b8b3201
alexytl <147929136+alexytl@users.noreply.github.com>
2025-02-10 13:10:41 +0900

    Update 9_Roadmap.py

--- pages/9_Roadmap.py
+++ pages/9_Roadmap.py
@@ -4,7 +4,7 @@ def main():
   st.title("Content Roadmap")
   st.header("Updates")
   st.markdown("2-4: Created new multipage app, added Playoffs page and Playoff Picture section")
-  st.markdown("2-9: Updated games and Playoff Picture Logic")
+  st.markdown("2-9: Updated games and Playoff Picture's status logic")
   st.header("Up Next")
   st.markdown("Sooner: Playoff Bracket, Playoff Matchup Guide, Pages for Schedule, Graphing and Advanced Statistics, Home/Away Splits, First/Second Matchup Splits, Away Rink Stats, Enhanced Graphing Capabilities")
   st.markdown("Later: Player Statistics, Predictive Model")
